4b1d3c19bbfa93e656a4ac14b9dc1ef1cc522651
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-04-11 12:46:04 +0000

    Add products app, models and fixtures

---
+++ products/admin.py
@@ -0,0 +1,7 @@
+"""Products Admin"""
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
