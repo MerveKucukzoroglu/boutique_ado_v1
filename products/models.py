65e38f320cb67fb51a5847f7882ba6c53fe67cbe
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-04-15 08:05:17 +0000

    Customize Admin

--- products/models.py
+++ products/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 class Category(models.Model):
     """Category model"""
+
+    class Meta:
+        """plural form of title"""
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
