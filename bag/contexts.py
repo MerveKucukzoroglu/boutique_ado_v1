2879c9ef58c6918da190f59d843218f951ca1d0d
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-04-23 18:05:15 +0000

    Add checkout logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -35,9 +35,9 @@ def bag_contents(request):
                     'size': size,
                 })
 
-    if total < settings.FREE_DELIVERY_TRESHOLD:
+    if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
-        free_delivery_delta = settings.FREE_DELIVERY_TRESHOLD - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
@@ -50,7 +50,7 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_treshold': settings.FREE_DELIVERY_TRESHOLD,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
